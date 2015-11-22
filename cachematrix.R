5057a232ea85d5f3b688faf7c8b1f7deeaa71213
jadudar <jdudar@gmail.com>
2015-11-22 10:58:16 -0500

    Added comments to cachematrix.R.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix returns a vector of functions for
+## interacting with a cached solution (inverted matrix)
+## for the argument x.
+##
+## set(a) stores a as the matrix to be inverted
+## get() returns the matrix to be inverted
+## setinv() stores the cached inverse
+## getniv() returns the cached inverse, or null if there is none.
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -18,7 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the inverse of the matrix stored
+## in the vector x. (The argument x is a vector as returned
+## by makeCacheMatrix.) For efficiency, cacheSolve returns
+## a cached value for
+## the inverse if it is available, and if not, cacheSolve
+## computes the inverse and stores it in the cache matrix
+## for subsequent use.
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
